5c4f21cd7b95d3da76a00a7afe6c92ff42a632fb
jason <1226130331@qq.com>
2022-11-25 14:37:11 +0800

    nvim-treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,10 +1,16 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 
-vim.cmd [[packadd packer.nvim]]
+local packer_bootstrap = ensure_packer()
+
 
 return require('packer').startup(function(use)
   -- packer
@@ -18,6 +24,14 @@ return require('packer').startup(function(use)
     },
     tag = 'nightly' -- optional, updated every week. (see issue #1193)
   }
+
   -- tokyonight colorscheme
   use 'folke/tokyonight.nvim'
+
+  -- nvim-treesitter
+  use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+  }
+
 end)
--- lua/setup.lua
+++ lua/setup.lua
@@ -2,4 +2,15 @@
 require("nvim-tree").setup()
 
 --tokyonight
-vim.cmd[[colorscheme tokyonight]]
+vim.cmd[[colorscheme tokyonight-storm]]
+
+--nvim-treesitter
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "lua", "python", "markdown"},
+  sync_install = false,
+  auto_install = true,
+  highlight = { 
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
